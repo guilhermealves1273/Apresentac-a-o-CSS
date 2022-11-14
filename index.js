dbd2e304cdea02ed724094b23e8df9baa406bf3e
guilhermealves1273 <guicardoso2002@hotmail.com>
2022-11-14 13:34:13 +0000

    ola

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function DarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark");
+    var body = document.body;
+    var botao = document.getElementById("botao");
+
+    if(body.classList.toggle("dark")) {
+        botao.innerHTML = "Light Mode";
+        botao.style.backgroundColor = "#fff";
+        botao.style.color = "#000";
+    } else {
+        botao.innerHTML = "Dark Mode";
+        botao.style.backgroundColor = "#000";
+        botao.style.color = "#fff";
     }
+}
 
